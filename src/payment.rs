17bcac4c813fb5b485e533504ee2ebc4586ba568
Ben Gale <bengale2007@googlemail.com>
2024-02-14 15:13:10 +0000

    Fixed clippy lint clippy::non_canonical_partial_ord_impl

--- src/payment.rs
+++ src/payment.rs
@@ -45,7 +45,7 @@ impl Eq for Payment {}
 
 impl PartialOrd for Payment {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.name.partial_cmp(&other.name)
+        Some(self.cmp(other))
     }
 }
 
